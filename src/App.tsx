fef3642bd5de7ef8fc9d6daf700f919235effdd7
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-05-06 10:19:53 -0700

    Add IPFS Desktop title in landing page

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>IPFS Desktop in Tauri</h1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
